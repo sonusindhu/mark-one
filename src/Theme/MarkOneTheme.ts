487dce4926fc6120f0c44bac862a47feef838317
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-10-24 13:57:40 -0400

    Add theme values from EAF
    I've migrated the CSS variables from EAF into our global theme. I also
    addded the `polished` library, and defined some of the base colors, etc.
    in separate constants so we can use the lighten/darken functions to
    modify the color palette as needed.

--- src/Theme/MarkOneTheme.ts
+++ src/Theme/MarkOneTheme.ts
@@ -1,4 +1,5 @@
 import { DefaultTheme } from 'styled-components';
+import { lighten, darken } from 'polished';
 
 type ColorRange = {
   light?: string;
@@ -25,16 +26,103 @@ export interface BaseTheme extends DefaultTheme {
   ws?: { [key: string]: string };
 }
 
+const COLOR = {
+  WHITE: '#ffffff',
+  BLACK: '#181818',
+  BLUE: '#0c6090',
+  GREEN: '#40DE40',
+  RED: '#ff4040',
+};
+
+const FONT = {
+  SANS: '"Open Sans", sans-serif',
+  MONO: '"Roboto Mono", monospace',
+};
+
+const WEIGHT = {
+  LIGHT: '300',
+  MEDIUM: '400',
+  BOLD: '600',
+};
+
 const MarkOneTheme: BaseTheme = {
   color: {
     background: {
-      light: '#ffffff',
-      subtle: '#f6f6f6',
-      medium: '#d2d2d2',
-      dark: '#909090',
-      darker: '#404040',
+      light: COLOR.WHITE,
+      subtle: darken(0.1, COLOR.WHITE),
+      medium: darken(0.25, COLOR.WHITE),
+      dark: darken(0.6, COLOR.WHITE),
+      darker: darken(0.75, COLOR.WHITE),
       accent: '#903030',
+      info: {
+        light: lighten(0.2, COLOR.BLUE),
+        medium: COLOR.BLUE,
+        dark: darken(0.2, COLOR.BLUE),
+      },
+      positive: {
+        light: lighten(0.2, COLOR.GREEN),
+        medium: COLOR.GREEN,
+        dark: darken(0.2, COLOR.GREEN),
+      },
+      negative: {
+        light: lighten(0.2, COLOR.RED),
+        medium: COLOR.RED,
+        dark: darken(0.2, COLOR.RED),
+      },
     },
+    text: {
+      light: lighten(0.75, COLOR.BLACK),
+      medium: lighten(0.35, COLOR.BLACK),
+      dark: COLOR.BLACK,
+      info: COLOR.BLUE,
+      positive: COLOR.GREEN,
+      negative: COLOR.RED,
+    },
+  },
+  font: {
+    body: {
+      family: FONT.SANS,
+      size: '1em',
+      weight: WEIGHT.MEDIUM,
+    },
+    data: {
+      family: FONT.MONO,
+      size: '0.9em',
+      weight: WEIGHT.BOLD,
+    },
+    bold: {
+      family: FONT.SANS,
+      size: '1em',
+      weight: WEIGHT.BOLD,
+    },
+    title: {
+      family: FONT.SANS,
+      size: '1.66em',
+      weight: WEIGHT.MEDIUM,
+    },
+    heading: {
+      family: FONT.SANS,
+      size: '1.33em',
+      weight: WEIGHT.LIGHT,
+    },
+  },
+  shadow: {
+    xlight: '0 2px 4px rgba(0,0,0,0.24)',
+    light: '0 4px 8px rgba(0,0,0,0.28)',
+    medium: '0 4px 16px 2px rgba(0,0,0,0.24)',
+  },
+  border: {
+    hairline: `1px solid ${COLOR.BLACK}`,
+    light: `2px solid ${lighten(0.75, COLOR.BLACK)}`,
+    heavy: `6px solid ${lighten(0.25, COLOR.BLACK)}`,
+  },
+  ws: {
+    zero: '0px',
+    xsmall: '5px',
+    small: '10px',
+    medium: '15px',
+    large: '25px',
+    xlarge: '40px',
   },
 };
 
