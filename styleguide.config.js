d2e684b26544a7ab4a216e39e814fcf198c5fbbf
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-10-24 11:01:14 -0400

    Replace default styleguide wrapper components
    This will wrap all of our component examples in the MarkOneWrapper,
    allowing us to reference the theme values through the React Context API.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 module.exports = {
   components: 'src/**/*.tsx',
-  getExampleFilename: (path) => (
-    path.replace(/\/src\//, '/examples/')
+  exampleMode: 'expand',
+  getExampleFilename: (filePath) => (
+    filePath.replace(/\/src\//, '/examples/')
       .replace(/\.tsx/, '.md')
   ),
   propsParser: require('react-docgen-typescript').withCustomConfig(
     './tsconfig.json'
   ).parse,
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, 'src/Theme/MarkOneWrapper.tsx'),
+  },
   webpackConfig: require('./webpackfile.js'),
-}
+};
