f7805bb28575b0795bfa8d6ad22d6904e7d78c8f
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-28 12:46:35 -0400

    Fix BaseTheme import path
    Update tsconfig file so that we can import BaseTheme from mark-one/Theme. We will eventually want to be able to import from just 'mark-one'.

--- src/Button/BasicButton.tsx
+++ src/Button/BasicButton.tsx
@@ -2,7 +2,7 @@ import React, {
   FunctionComponent, MouseEventHandler, ReactNode, ReactElement, useContext,
 } from 'react';
 import styled, { ThemeContext } from 'styled-components';
-import { BaseTheme } from '../Theme';
+import { BaseTheme } from 'mark-one/Theme';
 
 export interface ButtonProps {
   /** Text or components to be displayed on the button */
