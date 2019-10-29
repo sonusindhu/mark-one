036c12778529f21cdb5e5d3bc56369207ef3fbdf
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-29 10:25:52 -0400

    Add Cell component
    Add the functional component for cell with no styling for now.

--- src/Cell/Cell.tsx
+++ src/Cell/Cell.tsx
@@ -1,6 +1,28 @@
-import React, { ReactNode } from 'react';
+import React, { FunctionComponent, ReactElement, ReactNode } from 'react';
+import styled from 'styled-components';
 
 export interface CellProps {
   /** Text or components to be displayed in the cell */
   children: ReactNode;
 }
+
+const StyledCell = styled.td`
+  border: ;
+  font-family: ;
+  font-size: ;
+  padding: ;
+  text-align: ;
+`;
+
+const Cell: FunctionComponent<CellProps> = (props): ReactElement => {
+  const {
+    children,
+  } = props;
+  return (
+    <StyledCell>
+      {children}
+    </StyledCell>
+  );
+};
+
+export default Cell;
