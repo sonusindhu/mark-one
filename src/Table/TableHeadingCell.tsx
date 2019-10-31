d0d76b28ff95a4c4b5f8b198ffdf624ee07d50f8
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-31 10:00:59 -0400

    Add TableHeadingCell component
    Add functional component for TableHeadingCell

--- src/Table/TableHeadingCell.tsx
+++ src/Table/TableHeadingCell.tsx
@@ -1,6 +1,25 @@
-import { ReactNode } from 'react';
+import React, {
+  FunctionComponent, ReactElement, ReactNode, useContext,
+} from 'react';
+import { ThemeContext } from 'styled-components';
+import { BaseTheme } from 'mark-one';
 
 export interface TableHeadingCellProps {
   /** Text or components to be displayed in the table heading cell */
   children: ReactNode;
 }
+
+const TableHeadingCell: FunctionComponent<TableHeadingCellProps> = (props):
+ReactElement => {
+  const {
+    children,
+  } = props;
+  const theme: BaseTheme = useContext(ThemeContext);
+  return (
+    <StyledTableHeadingCell theme={theme}>
+      {children}
+    </StyledTableHeadingCell>
+  );
+};
+
+export default TableHeadingCell;
