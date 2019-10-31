7d85b5cc6ae51d4716efa119d816106f4c8485c5
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-31 10:12:17 -0400

    Include optional clickHandler
    Add an optional clickHandler prop for TableHeadingCellProps

--- src/Table/TableHeadingCell.tsx
+++ src/Table/TableHeadingCell.tsx
@@ -1,14 +1,19 @@
 import React, {
-  FunctionComponent, ReactElement, ReactNode, useContext,
+  FunctionComponent, MouseEventHandler, ReactElement, ReactNode, useContext,
 } from 'react';
-import { ThemeContext } from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 import { BaseTheme } from 'mark-one';
 
 export interface TableHeadingCellProps {
   /** Text or components to be displayed in the table heading cell */
   children: ReactNode;
+  /** Function to call on click event */
+  clickHandler?: MouseEventHandler;
 }
 
+const StyledTableHeadingCell = styled.td`
+
+`;
 const TableHeadingCell: FunctionComponent<TableHeadingCellProps> = (props):
 ReactElement => {
   const {
