27944af05942d74e9b3e45891f2bdd5ab69368ad
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-10-31 15:55:15 -0400

    Add StyledTableHead
    Include default background color for the styledtablehead.

--- src/Table/TableHead.tsx
+++ src/Table/TableHead.tsx
@@ -1,9 +1,27 @@
 import React, {
   FunctionComponent, ReactElement,
 } from 'react';
+import styled from 'styled-components';
+import TableRow from './TableRow';
+
+export interface TableHeadProps {
+  /** Our TableRow functional component serves as the children for TableHead */
+  children: TableRow;
+}
+
+const StyledTableHead = styled.thead`
+  background-color: theme.color.background.medium;
+`;
 
 const TableHead: FunctionComponent<TableHeadProps> = (props): ReactElement => {
-
+  const {
+    children,
+  } = props;
+  return (
+    <StyledTableHead>
+      {children}
+    </StyledTableHead>
+  );
 };
 
 export default TableHead;
