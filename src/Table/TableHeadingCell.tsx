ed93507e31c9ec94f3d652af8d9e0189e3befb94
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-31 10:45:57 -0400

    Add default styling for StyledTableHeadingCell
    Include font weight and text align as styling for StyledTableHeadingCell

--- src/Table/TableHeadingCell.tsx
+++ src/Table/TableHeadingCell.tsx
@@ -14,7 +14,8 @@ export interface TableHeadingCellProps {
 }
 
 const StyledTableHeadingCell = styled.td`
-
+  font-weight: ${({ theme }): string => (theme.font.bold.weight)};
+  text-align: 'center';
 `;
 const TableHeadingCell: FunctionComponent<TableHeadingCellProps> = (props):
 ReactElement => {
