7388f88a79e5ec7896a768994d537c728d5fd75d
Natalyn Yu <nmiddletonyu@g.harvard.edu>
2019-10-31 12:46:32 -0400

    Fix export file path
    Update export file path so that TableHeadingCell is actually being exported. File path was incorrect before and was exporting TableRow.

--- src/Table/index.ts
+++ src/Table/index.ts
@@ -1,4 +1,4 @@
 export { default as Table } from './Table';
 export { default as TableCell } from './TableCell';
 export { default as TableRow } from './TableRow';
-export { default as TableHeadingCell } from './TableRow';
+export { default as TableHeadingCell } from './TableHeadingCell';
