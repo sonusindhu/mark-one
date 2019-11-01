1c9499cf3578506aaa28888deca2361ec2778543
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-01 13:20:59 -0400

    Include TableBody in the Table.md file
    Include TableBody functional component in the markdown file.

--- src/Table/index.ts
+++ src/Table/index.ts
@@ -4,4 +4,3 @@ export { default as TableRow } from './TableRow';
 export { default as TableHead } from './TableHead';
 export { default as TableHeadingCell } from './TableHeadingCell';
 export { default as TableBody } from './TableBody';
-
