460d35b7332545dcb0e628c5eb1529c277b424c4
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-01 09:55:51 -0400

    Add export of TableBody to Table/index.ts
    Export TableBody from Table/index.ts so that we can later import TableBody from mark-one

--- src/Table/index.ts
+++ src/Table/index.ts
@@ -3,4 +3,5 @@ export { default as TableCell } from './TableCell';
 export { default as TableRow } from './TableRow';
 export { default as TableHead } from './TableHead';
 export { default as TableHeadingCell } from './TableHeadingCell';
+export { default as TableBody } from './TableBody';
 
