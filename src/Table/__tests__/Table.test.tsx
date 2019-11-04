13a046b72bae100095e2c2f76b142be372523f11
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-04 12:32:20 -0500

    Include test case for TableRow and TableHeading
    Test that TableHeading includes test to make sure that the heading contains text. Also include a test to make sure that the table row renders each cell.

--- src/Table/__tests__/Table.test.tsx
+++ src/Table/__tests__/Table.test.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import { render } from 'test-utils';
+import { render, cleanup } from 'test-utils';
+import { strictEqual } from 'assert';
 import TableCell from '../TableCell';
+import TableBody from '../TableBody';
+import TableHead from '../TableHead';
+import TableHeadingCell from '../TableHeadingCell';
+import TableRow from '../TableRow';
 import Table from '../Table';
 
 describe('Table Components', function () {
   let getByText;
+  let getAllByRole;
   beforeEach(function () {
-    ({ getByText } = render(
+    ({ getByText, getAllByRole } = render(
       <Table>
-        <tbody>
-          <tr>
-            <TableCell>Hello World</TableCell>
-          </tr>
-        </tbody>
+        <TableHead>
+          <TableRow>
+            <TableHeadingCell scope="col">ID</TableHeadingCell>
+            <TableHeadingCell scope="col">First Name</TableHeadingCell>
+            <TableHeadingCell scope="col">Last Name</TableHeadingCell>
+          </TableRow>
+        </TableHead>
+        <TableBody isScrollable>
+          <TableRow isStriped>
+            <TableCell>1</TableCell>
+            <TableCell>Lucy</TableCell>
+            <TableCell>Bernstein</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell>Jess</TableCell>
+            <TableCell>Win</TableCell>
+          </TableRow>
+          <TableRow isStriped>
+            <TableCell>3</TableCell>
+            <TableCell>Sam</TableCell>
+            <TableCell>Anderson</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>4</TableCell>
+            <TableCell>Judy</TableCell>
+            <TableCell>Monson</TableCell>
+          </TableRow>
+        </TableBody>
       </Table>
     ));
   });
+  afterEach(function () {
+    cleanup();
+  });
   describe('Table Cell', function () {
     it('contains text', function () {
-      getByText('Hello World');
+      getByText('1');
+    });
+  });
+  describe('Table Heading Cell', function () {
+    it('contains text', function () {
+      getByText('ID');
+    });
+  });
+  describe('Table Row', function () {
+    it('renders every table cell', function () {
+      const cellCount = getAllByRole('cell').length;
+      strictEqual(cellCount, 12);
     });
   });
 });
