6730a8efca99a6c2713212fbdfb7995da47a175e
Robert Main <rmain@seas.harvard.edu>
2019-11-06 10:11:24 -0500

    Props Documentation
    Add documentation for component props

--- src/Layout/PageBody.tsx
+++ src/Layout/PageBody.tsx
@@ -3,7 +3,9 @@ import styled, { withTheme } from 'styled-components';
 import { BaseTheme } from 'mark-one';
 
 export interface PageBodyProps {
+  /** Page contents */
   children?: ReactNode;
+  /** The theme this component should use for styling */
   theme: BaseTheme,
 }
 
