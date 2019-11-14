68311608e886b0fb6225dea291deaed593ef2add
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-11-14 11:58:25 -0500

    Create overview, guidelines, and sections
    To integrate Vittorio's work on the interaction guidelines, I've added
    some logic to parse through a `guidelines` directory and add any
    markdown files in there to the styleguide output, under a "Guidelines"
    heading.
    
    Expanding on that, I configured it to generate separate pages for each
    sub-section of components, and to add our README and CONTRIBUTING
    documents as the first entries in the library. I think this should make
    the whole document a little more user friendly.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,15 +1,83 @@
 const path = require('path');
+const fs = require('fs');
+
+const guidelinesSections = fs.readdirSync(
+  path.join(__dirname, 'guidelines'),
+  {
+    withFileTypes: true,
+  }
+).reduce((list, page) => {
+  const { name: fileName } = page;
+  if (/\.md$/.test(fileName)) {
+    const name = fileName.replace(/\.md$/, '');
+    const content = path.join('guidelines', fileName);
+    const sectionObject = {
+      name,
+      content,
+    };
+    list.push(sectionObject);
+  }
+  return list;
+}, []);
+
+const componentSections = fs.readdirSync(
+  path.join(__dirname, 'src'),
+  {
+    withFileTypes: true,
+  }
+).reduce((list, component) => {
+  if (component.isDirectory() && component.name !== '__tests__') {
+    const { name } = component;
+    const sectionObject = {
+      name,
+      components: `src/${name}/*.tsx`,
+      ignore: '*.test.tsx',
+    };
+    const overviewFile = path.join('examples', name, 'README.md');
+    if (fs.existsSync(overviewFile)) {
+      sectionObject.content = overviewFile;
+    }
+    list.push(sectionObject);
+  }
+  return list;
+}, []);
+
+const sections = [
+  {
+    name: 'Overview',
+    content: 'README.md',
+    sectionDepth: 1,
+    sections: [
+      {
+        name: 'Contributing',
+        content: 'CONTRIBUTING.md',
+      },
+    ],
+  },
+  {
+    name: 'Interaction Guidelines',
+    sections: guidelinesSections,
+    pageDepth: 1,
+  },
+  ...componentSections,
+];
 
 module.exports = {
   components: 'src/**/*.tsx',
+  defaultExample: 'examples/default.md',
   exampleMode: 'expand',
-  getExampleFilename: (filePath) => (
-    filePath.replace(/\/src\//, '/examples/')
-      .replace(/\.tsx/, '.md')
-  ),
+  getExampleFilename: (filePath) => {
+    const testFile = filePath.replace(/\/src\//, '/examples/')
+      .replace(/\.tsx$/, '.md');
+    if (fs.existsSync(testFile)) {
+      return testFile;
+    }
+  },
+  pagePerSection: true,
   propsParser: require('react-docgen-typescript').withCustomConfig(
     './tsconfig.json'
   ).parse,
+  sections,
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/Theme/MarkOneWrapper.tsx'),
   },
