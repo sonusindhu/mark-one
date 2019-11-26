bb66d030d3903e097646e1d1142c460e7eb73636
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-26 10:59:19 -0500

    Add basic structure for NavList component
    Set up directory structure and basic NavList component

---
+++ src/Navigation/NavList.tsx
@@ -0,0 +1,18 @@
+import React, {
+  FunctionComponent, ReactElement, ReactNode,
+} from 'react';
+import styled from 'styled-components';
+
+export interface NavListProps {
+  children: ReactNode;
+}
+
+const StyledNavList = styled.nav`
+
+`;
+
+const NavList: FunctionComponent<NavListProps> = (props): ReactElement => {
+
+};
+
+export default NavList;
