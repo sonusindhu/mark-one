99030f1a365c1eae4ec17fc7fb705f8a57059bed
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-12-02 16:10:59 -0500

    Use withTheme in NavItem component
    Instead of using BaseTheme, use withTheme for NavItem component in order to use get by data-testid in NavList test

--- src/Navigation/__tests__/NavList.test.tsx
+++ src/Navigation/__tests__/NavList.test.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render, cleanup } from 'test-utils';
+import convert from 'color-convert';
+import { strictEqual } from 'assert';
 import NavList from '../NavList';
 import NavItem from '../NavItem';
+import MarkOneTheme from '../../Theme/MarkOneTheme';
 
 describe('Navigation Components', function () {
   let getByText;
+  let getAllByRole;
+  let getByTestId;
   beforeEach(function () {
-    ({ getByText } = render(
+    ({ getByText, getAllByRole, getByTestId } = render(
       <NavList>
         <NavItem isActive={false}>Tab 1</NavItem>
-        <NavItem isActive>Tab 2</NavItem>
+        <NavItem isActive data-testid="activeNavItem">Tab 2</NavItem>
         <NavItem isActive={false}>Tab 3</NavItem>
       </NavList>
     ));
@@ -17,9 +22,24 @@ describe('Navigation Components', function () {
   afterEach(function () {
     cleanup();
   });
+  describe('Nav List', function () {
+    it('renders every nav item', function () {
+      const listItemCount = getAllByRole('listitem').length;
+      strictEqual(listItemCount, 3);
+    });
+  });
   describe('Nav Item', function () {
     it('contains text', function () {
       getByText('Tab 1');
+    });
+    it('renders a nav item with the light theme background color when isActive is true', function () {
+      const navItem = getByTestId('activeNavItem');
+      const style = window.getComputedStyle(navItem);
+      const [red, green, blue] = convert.hex.rgb(
+        MarkOneTheme.color.background.light as string
+      );
+      const convertExpectedToRGB = `rgb(${red}, ${green}, ${blue})`;
+      strictEqual(style.backgroundColor, convertExpectedToRGB);
     });
   });
 });
