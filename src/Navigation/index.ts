75ff5bf511fb96f2c257399c28745525690304ec
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-12-02 10:34:35 -0500

    Export NavList and NavItems from index.ts
    Add Navigation items as exports to index.ts

---
+++ src/Navigation/index.ts
@@ -0,0 +1,2 @@
+export { default as NavList } from './NavList';
+export { default as NavItem } from './NavItem';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './Button';
 export * from './Layout';
+export * from './Navigation';
 export * from './Theme';
 export * from './Tables';
 export * from './Layout';
