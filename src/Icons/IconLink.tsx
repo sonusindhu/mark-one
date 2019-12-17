23e4928728bd9b71bf868156863240463de5a677
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-12-17 12:42:35 -0500

    Add documentation for props

--- src/Icons/IconLink.tsx
+++ src/Icons/IconLink.tsx
@@ -1,5 +1,6 @@
 import React, {
-  FunctionComponent, ReactElement, useContext, MouseEventHandler, ReactText,
+  FunctionComponent, ReactElement, useContext, MouseEventHandler,
+  ReactText, ReactNode,
 } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import { BaseTheme } from 'Theme';
@@ -7,8 +8,14 @@ import { BaseTheme } from 'Theme';
 export interface IconLinkProps {
   /** Function to call on click event */
   clickHandler: MouseEventHandler;
+  /** Specifies the Font Awesome icon name */
   icon: ReactText;
+  /** Specifies the tooltip text */
   title: ReactText;
+  /** Specifies the alt text for screen readers */
+  alt: ReactText;
+  /** Allows layering of Font Awesome icons, which is possible through nesting */
+  children?: ReactNode;
 }
 
 const StyledIconLink = styled.a`
@@ -21,6 +28,8 @@ const IconLink: FunctionComponent<IconLinkProps> = (props): ReactElement => {
     clickHandler,
     icon,
     title,
+    alt,
+    children,
   } = props;
   const theme: BaseTheme = useContext(ThemeContext);
   return (
@@ -28,8 +37,11 @@ const IconLink: FunctionComponent<IconLinkProps> = (props): ReactElement => {
       onClick={clickHandler}
       icon={icon}
       title={title}
+      alt={alt}
       theme={theme}
-    />
+    >
+      { children }
+    </StyledIconLink>
   );
 };
 
