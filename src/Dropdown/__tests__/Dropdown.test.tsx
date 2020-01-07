0c2136ad765dc9768f9414e9a292cfd3e61476f3
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-01-07 09:08:52 -0500

    Add additional tests for dropdown component
    1.It contains the number of elements expected
    2.Each element has the correct value and label
    3.The default value is what you expect

--- src/Dropdown/__tests__/Dropdown.test.tsx
+++ src/Dropdown/__tests__/Dropdown.test.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from 'test-utils';
 import { spy } from 'sinon';
-import assert from 'assert';
+import assert, { strictEqual, deepStrictEqual } from 'assert';
 import Dropdown from '../Dropdown';
 
 describe('Dropdown', function () {
   let getByText;
   let changeSpy;
+  let getAllByRole;
+  const options = [
+    {
+      value: 'all', label: 'All',
+    },
+    {
+      value: 'fall', label: 'Fall',
+    },
+    {
+      value: 'spring', label: 'Spring',
+    },
+  ];
   beforeEach(function () {
     changeSpy = spy();
-    ({ getByText } = render(
+    ({ getByText, getAllByRole } = render(
       <Dropdown
-        options={[
-          {
-            value: 'all', label: 'All',
-          },
-          {
-            value: 'fall', label: 'Fall',
-          },
-          {
-            value: 'spring', label: 'Spring',
-          },
-        ]}
+        options={options}
         value="fall"
         name="semesters"
         onChange={changeSpy}
@@ -38,5 +40,22 @@ describe('Dropdown', function () {
   it('calls the change handler when changed', function () {
     fireEvent.change(document.getElementsByName('semesters')[0]);
     assert.equal(changeSpy.callCount, 1);
+  });
+  it('contains the expected elements', function () {
+    const dropdownOptionsCount = getAllByRole('option').length;
+    strictEqual(dropdownOptionsCount, 3);
+  });
+  it('renders the correct default value', function () {
+    const dropdown = document.getElementsByName('semesters')[0] as HTMLSelectElement;
+    const defaultValue = dropdown.value;
+    strictEqual(defaultValue, 'fall');
+  });
+  it('contains the correct value and label for each dropdown item', function () {
+    const dropdown = document.getElementsByName('semesters')[0] as HTMLSelectElement;
+    const optionsFound = Array.from(dropdown.options).map((element) => ({
+      value: element.value,
+      label: element.label,
+    }));
+    deepStrictEqual(optionsFound, options);
   });
 });
