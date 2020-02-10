f9703dfcfef76e83b7d30cf7a1f89c602241c046
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-02-10 11:24:33 -0500

    Only automate develop branch
    After further reflection and discussion, we should not be automating
    production releases -- those should be done manually. Instead, we'll
    publish every merge to develop as a `-develop` pre-release.

--- .travis_version_bump.sh
+++ .travis_version_bump.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# Set the user name and email to match the API token holder
-git config --global user.email $DEV_TEAM_EMAIL 
-git config --global user.name "SEAS Computing Robot"
-git config --global push.default matching
-
-# Get the credentials from a file
-git config credential.helper "store --file=.git/credentials"
-
-# This associates the API Key with the account
-echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-
-git checkout -- .
-  
-# %s is the placeholder for the created tag
-if [[ $TRAVIS_BRANCH == master ]]; then
-  SEMVER=minor
-fi
-
 if [[ "$TRAVIS_BRANCH" == develop ]]; then 
-  SEMVER=patch
+
+  # Set the user name and email to match the API token holder
+  git config --local user.email $DEV_TEAM_EMAIL 
+  git config --local user.name "SEAS Computing Robot"
+  git config --local push.default matching
+
+  # Get the credentials from a file
+  git config credential.helper "store --file=.git/credentials"
+
+  # This associates the API Key with the account
+  echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+  
+  # Local branch must be clean before bumping version
+  git checkout -- .
+
+  npm version prerelease --preid=develop -m "[skip ci] Automatic bump to %s"
+  git push --tags origin HEAD:$TRAVIS_BRANCH
 fi
 
-if [[ "$SEMVER" != "" ]]; then
-  npm version $SEMVER -m "automatic bump to %s [skip ci]"
-fi
-
-git push --tags origin HEAD:$TRAVIS_BRANCH
 
 # Adapted from https://dev.to/jeffreymfarley/deploy-atomically-with-travis--npm-68b
 # By Jeff Farley
