a36b12ea9ecfbec298e21f5a632b0bd5cdd4ba49
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-02-20 10:03:51 -0500

    Export all button types from index.ts

--- src/Button/index.ts
+++ src/Button/index.ts
@@ -1,1 +1,5 @@
 export { default as BasicButton } from './BasicButton';
+export { default as BorderlessButton } from './BorderlessButton';
+export { default as DangerButton } from './DangerButton';
+export { default as PrimaryButton } from './PrimaryButton';
+export { default as SecondaryButton } from './SecondaryButton';
