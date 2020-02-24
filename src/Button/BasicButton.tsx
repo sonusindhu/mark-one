cfa098f03782cc9f0fa3bd96a3fffd7d43e3a7cd
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-02-24 12:31:43 -0500

    Add button props and styles to BasicButton component
    Props and styles no longer need to be separated into their own files, since we are now having 1 button component that users can pass a variant prop to instead of having several button components.

--- src/Button/BasicButton.tsx
+++ src/Button/BasicButton.tsx
@@ -1,23 +1,64 @@
 import React, {
-  FunctionComponent, ReactElement, useContext,
+  FunctionComponent,
+  ReactElement,
+  useContext,
+  ReactNode,
+  MouseEventHandler,
 } from 'react';
-import { ThemeContext } from 'styled-components';
+import { VARIANT } from 'Theme/MarkOneTheme';
+import styled, { ThemeContext } from 'styled-components';
 import { BaseTheme } from '../Theme';
-import { ButtonProps } from './ButtonProps';
-import { StyledButton } from './StyledButton';
+
+export interface ButtonProps {
+  /** Text or components to be displayed on the button */
+  children?: ReactNode;
+  /** Function to call on click event */
+  onClick: MouseEventHandler;
+  /** If true, button won't fire */
+  disabled?: boolean;
+  /** Allows you to pass in style properties for the element */
+  theme?: object;
+  /** Allows you to pass in a variant property from the VARIANT enum */
+  variant: VARIANT;
+}
+
+// If background is primary or danger, have light text (theme.color.text.light)
+// If background is light, have dark text (theme.color.text.base)
+const StyledButton = styled.button<ButtonProps>`
+  background: ${({ theme, variant }): string => (theme.color.background[variant].medium)};
+  border-color: ${({ theme, variant }): string => (theme.color.background[variant].medium)};
+  border-style: solid;
+  border-width: 1px;
+  color: ${({ theme, variant }): string => (theme.color.text[variant])};
+  cursor: pointer;
+  font-size: ${({ theme }): string => (theme.font.body.size)};
+  font-weight: ${({ theme }): string => (theme.font.body.weight)};
+  padding: ${({ theme }): string => (theme.ws.xsmall + ' ' + theme.ws.small)};
+  &:hover {
+    background: ${({ theme, variant }): string => (theme.color.background[variant].dark)};
+    border-color: ${({ theme, variant }): string => (theme.color.background[variant].dark)};
+    color: ${({ theme, variant }): string => (theme.color.text[variant])};
+  }
+`;
+
+StyledButton.defaultProps = {
+  variant: VARIANT.BASE,
+};
 
 const BasicButton: FunctionComponent<ButtonProps> = (props): ReactElement => {
   const {
     children,
-    clickHandler,
+    onClick,
     disabled,
+    variant,
   } = props;
   const theme: BaseTheme = useContext(ThemeContext);
   return (
     <StyledButton
-      onClick={clickHandler}
+      onClick={onClick}
       theme={theme}
       disabled={disabled}
+      variant={variant}
     >
       { children }
     </StyledButton>
