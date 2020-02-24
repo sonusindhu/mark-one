d274cf9ebd7d9276f923d6854b1879f7648f71b2
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-02-24 12:29:04 -0500

    Remove exports of Button variants

--- src/Button/index.ts
+++ src/Button/index.ts
@@ -1,5 +1,2 @@
 export { default as BasicButton } from './BasicButton';
 export { default as BorderlessButton } from './BorderlessButton';
-export { default as DangerButton } from './DangerButton';
-export { default as PrimaryButton } from './PrimaryButton';
-export { default as SecondaryButton } from './SecondaryButton';
