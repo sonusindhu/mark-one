10369ab49569379233e03b881e4369c03e4fa16e
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-02-24 09:10:00 -0500

    Add type for getByText in Validation Error test file

--- src/Forms/__tests__/ValidationErrorMessage.test.tsx
+++ src/Forms/__tests__/ValidationErrorMessage.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, cleanup } from 'test-utils';
+import {
+  render,
+  cleanup,
+  Matcher,
+  NormalizerFn,
+} from 'test-utils';
 import ValidationErrorMessage from '../ValidationErrorMessage';
 
+type ReactGetByText = (
+  text: Matcher,
+  options?: {
+    selector?: string;
+    exact?: boolean;
+    ignore?: string|boolean;
+    normalizer?: NormalizerFn;
+  }) => HTMLElement;
+
 describe('Validation Error Message', function () {
-  let getByText;
+  let getByText: ReactGetByText;
   beforeEach(function () {
     ({ getByText } = render(
       <ValidationErrorMessage>
