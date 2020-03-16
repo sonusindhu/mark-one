1726e0f5c628287bdad80649e9791e8a7fe8600a
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-03-16 16:14:28 -0400

    Fix various formatting for Text Input component
    Using CSS grid, fix spacing between label and text input per Vittorio and Jonathan's styling suggestions. Set align-items to 'end' on the grid container to fix vertical stretching of the input field. Use minmax for the grid-template-rows to solve for cases where the description and/or the error spans multiple lines (previously, this would cause spacing issues - specifically, this would create large gaps between each row). Fix padding using the 'gap' property. Add function to determine grid-template-areas.

--- src/Forms/Label.tsx
+++ src/Forms/Label.tsx
@@ -13,6 +13,8 @@ export enum POSITION {
 }
 
 export interface StyledLabelProps {
+  /** If true, label will be visible */
+  isLabelVisible?: boolean;
   /** Allows you to pass in a label position property from the POSITION enum */
   labelPosition?: POSITION;
   /** Specifies the id for the label */
@@ -22,6 +24,8 @@ export interface StyledLabelProps {
 export interface StyledLabelTextProps {
   /** If true, label will be visible */
   isLabelVisible?: boolean;
+  /** Allows you to pass in a label position property from the POSITION enum */
+  labelPosition?: POSITION;
 }
 
 export interface LabelProps {
@@ -35,28 +39,40 @@ export interface LabelProps {
   isLabelVisible?: boolean;
 }
 
+const generateGrid = (labelPosition, isLabelVisible): string => {
+  if (!isLabelVisible) {
+    return `"i i i"
+            "e e e"`;
+  }
+  if (labelPosition === POSITION.TOP) {
+    return `"l l l"
+            "i i i"
+            "e e e"`;
+  }
+  return `"l i i"
+          ". e e"`;
+};
+
 const StyledLabel = styled.label<StyledLabelProps>`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: ${({ labelPosition }): string => (
-    labelPosition === POSITION.TOP
-      ? 'repeat(3, 1fr)'
-      : 'repeat(2, 1fr)'
-  )};
-  grid-template-areas: ${({ labelPosition }): string => (
-    labelPosition === POSITION.TOP
-      ? `"l l l"
-        "i i i"
-        "e e e"`
-      : `"l i i"
-        ". e e"`
+  grid-template-rows: 1fr minmax(1em, max-content);
+  grid-template-areas: ${({ labelPosition, isLabelVisible }): string => (
+    generateGrid(labelPosition, isLabelVisible)
   )};
   margin: ${({ theme }): string => (theme.ws.small)};
+  align-items: end;
+  gap: ${({ theme }): string => (theme.ws.xsmall) + ' ' + (theme.ws.xsmall)};
 `;
 
 const StyledLabelText = styled.span<StyledLabelTextProps>`
   display: ${({ isLabelVisible }): string => (isLabelVisible ? 'inline' : 'none')};
   grid-area: l;
+  justify-self: ${({ labelPosition }): string => (
+    labelPosition === POSITION.TOP
+      ? 'start'
+      : 'end'
+  )};
 `;
 
 const Label: FunctionComponent<LabelProps> = (props): ReactElement => {
@@ -73,9 +89,11 @@ const Label: FunctionComponent<LabelProps> = (props): ReactElement => {
       htmlFor={htmlFor}
       labelPosition={labelPosition}
       theme={theme}
+      isLabelVisible={isLabelVisible}
     >
       <StyledLabelText
         isLabelVisible={isLabelVisible}
+        labelPosition={labelPosition}
       >
         {label}
       </StyledLabelText>
@@ -85,6 +103,11 @@ const Label: FunctionComponent<LabelProps> = (props): ReactElement => {
 };
 
 Label.defaultProps = {
+  labelPosition: POSITION.LEFT,
+  isLabelVisible: true,
+};
+
+StyledLabel.defaultProps = {
   labelPosition: POSITION.LEFT,
   isLabelVisible: true,
 };
