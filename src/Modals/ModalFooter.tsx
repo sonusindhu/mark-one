5f9294310c7b2411cd65713a5bf558ab740e7ca2
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-04-06 16:33:42 -0400

    add modal footer component

---
+++ src/Modals/ModalFooter.tsx
@@ -0,0 +1,30 @@
+import { ReactElement, ReactNode } from 'react';
+import styled, { withTheme } from 'styled-components';
+
+interface ModalFooterProps {
+  /**
+   * the content of the Modal Footer
+   */
+  children: ReactNode;
+}
+
+
+const styledModalFooter = styled.div<ModalFooterProps>`
+  background-color: ${({ theme }): string => (theme.color.background.subtle)};
+  border-top: ${({ theme }): string => (theme.border.light)};
+  display: flex;
+  flex-direction: reverse-row;
+  justify-content: space-between;
+  padding: ${({ theme }): string => `${theme.ws.small} ${theme.ws.medium}`};
+  width: 100%;
+`;
+
+declare type ModalFooter = ReactElement<ModalFooterProps>;
+
+/**
+ * @component ModalFooter
+ * Used within the [[Modal]] component to render a separated bottom section,
+ * typically containing buttons for save, cancel, etc.
+ */
+export const ModalFooter = withTheme(styledModalFooter);
+export default ModalFooter;
