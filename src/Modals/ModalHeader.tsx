9d43d4a4a0cb842995e329b132e88f1b31e99b31
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-04-08 11:20:55 -0400

    Create Modal Header
    Adds a Header bar to the modal, which renders any `children` text inside
    a slightly modified `<SectionHeading>` component (removes the bottom
    border). Also accepts an optional `closeButtonHandler` prop: if present,
    it renders a close button in the top corner; if not, it doesn't render
    the button and closing functionality might be handled in another way.

---
+++ src/Modals/ModalHeader.tsx
@@ -0,0 +1,73 @@
+import React, {
+  ReactElement,
+  ReactNode,
+  FunctionComponent,
+  useContext,
+} from 'react';
+import styled, { ThemeContext } from 'styled-components';
+import { BorderlessButton } from 'Buttons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { VARIANT, BaseTheme } from 'Theme';
+import { SectionHeading } from 'Headings';
+
+interface ModalHeaderProps {
+  /**
+   * the Title to display in the Modal Header
+   */
+  children: ReactNode;
+  /**
+   * A handler to pass to the close button. If omitted, no close button will be
+   * rendered
+   */
+  closeButtonHandler?: () => void;
+}
+
+const ModalTitle = styled(SectionHeading)`
+  border-bottom: none;
+`;
+
+const StyledModalHeader = styled.div<{theme: BaseTheme}>`
+  background-color: ${({ theme }): string => (theme.color.background.subtle)};
+  border-bottom: ${({ theme }): string => (theme.border.light)};
+  display: flex;
+  justify-content: space-between;
+  padding: ${({ theme }): string => `${theme.ws.small} ${theme.ws.medium}`};
+  width: 100%;
+`;
+
+/**
+ * @component ModalHeader
+ * Utility subcomponent for the [[Modal]]. Adds a title bar with the child text
+ * in an `<h2>` tag. If an optional `closeButtonHandler` is included, will also
+ * render a `X` close button.
+ */
+declare type ModalHeader = ReactElement<ModalHeaderProps>;
+const ModalHeader: FunctionComponent<ModalHeaderProps> = ({
+  closeButtonHandler,
+  children,
+}): ReactElement<ModalHeaderProps> => {
+  const markOneTheme = useContext(ThemeContext);
+  return (
+    <StyledModalHeader
+      theme={markOneTheme}
+    >
+      <ModalTitle>
+        {children}
+      </ModalTitle>
+      {closeButtonHandler
+      && (
+        <BorderlessButton
+          variant={VARIANT.NEGATIVE}
+          onClick={closeButtonHandler}
+        >
+          <FontAwesomeIcon
+            title="Close Modal"
+            icon={faTimes}
+          />
+        </BorderlessButton>
+      )}
+    </StyledModalHeader>
+  );
+};
+export default ModalHeader;
---
+++ src/Modals/__tests__/ModalHeader.test.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { strictEqual } from 'assert';
+import { spy, SinonSpy } from 'sinon';
+import {
+  render,
+  fireEvent,
+  BoundFunction,
+  QueryByBoundAttribute,
+} from 'test-utils';
+import Modal from '../Modal';
+import ModalHeader from '../ModalHeader';
+
+describe('Modal Header', function () {
+  describe('closeButtonHandler prop', function () {
+    let cbHandler: SinonSpy;
+    beforeEach(function () {
+      cbHandler = spy();
+    });
+    context('When a handler is passed in', function () {
+      let queryByTitle: BoundFunction<QueryByBoundAttribute>;
+      beforeEach(function () {
+        ({ queryByTitle } = render(
+          <Modal
+            isVisible
+          >
+            <ModalHeader
+              closeButtonHandler={cbHandler}
+            >
+              Modal Title
+            </ModalHeader>
+          </Modal>
+        ));
+      });
+      it('Should render the close button', function () {
+        const closeButton = queryByTitle('Close Modal');
+        // strictEqual(modal, null) was causing an OOM crash when the test
+        // failed, but this works fine
+        strictEqual(closeButton === null, false);
+      });
+      it('Should call the handler when the button is clicked', function () {
+        const closeButton = queryByTitle('Close Modal');
+        strictEqual(cbHandler.callCount, 0);
+        fireEvent.click(closeButton);
+        strictEqual(cbHandler.callCount, 1);
+      });
+    });
+    context('When no handler is passed in', function () {
+      it('Should not render the close button', function () {
+        const { queryByTitle } = render(
+          <Modal
+            isVisible
+          >
+            displayed
+          </Modal>
+        );
+        const modal = queryByTitle('Close Modal');
+        strictEqual(modal === null, true);
+      });
+    });
+  });
+  describe('Title Text', function () {
+    it('Renders the title text in an h2', function () {
+      const { getByText } = render(
+        <Modal
+          isVisible
+        >
+          <ModalHeader>
+            Title Text
+          </ModalHeader>
+        </Modal>
+      );
+      const title = getByText('Title Text');
+      strictEqual(title.tagName, 'H2');
+    });
+  });
+});
