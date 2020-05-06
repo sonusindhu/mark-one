6b8359a2ba03e316f0f89da25e3957440b9da2f2
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-05-06 14:19:09 -0400

    Allow override of mouse-over highlighting
    Again, adds a feature that's somewhat specifc to the course instance
    table. The Semester header shouldn't be highlighted on mouseover, so we
    need a way to disable that feature.

--- src/Tables/TableRow.tsx
+++ src/Tables/TableRow.tsx
@@ -6,17 +6,30 @@ import styled, { withTheme } from 'styled-components';
 export interface TableRowProps {
   /** Cell components to be displayed in the row */
   children: ReactNode;
-  /** Controls whether the background of the row has a darker background color */
+  /**
+   * Controls whether the background of the row has a darker background color
+   * Inteded for use in long tables that help differentiate even/odd rows
+   * */
   isStriped?: boolean;
+  /**
+   * By default, rows will highlight on hover. Setting this to true will
+   * disable that behavior
+   */
+  noHighlight?: boolean;
+  /** The application theme */
+  theme: BaseTheme;
 }
 
 const TableRow = styled.tr<TableRowProps>`
-  background: ${({ theme, isStriped }): string => (isStriped ? theme.color.background.subtle : theme.color.background.light)};
+  background: ${({ theme, isStriped }): string => (
+    isStriped
+      ? theme.color.background.subtle
+      : theme.color.background.light)};
   &:hover {
-    background: ${({ theme }): string => (theme.color.background.medium)};
-  }
-  th {
-    background-color: ${({ theme }): string => (theme.color.background.medium)};
+    background: ${({ theme, noHighlight }): string => (
+    noHighlight
+      ? null
+      : theme.color.background.medium)};
   }
 `;
 
