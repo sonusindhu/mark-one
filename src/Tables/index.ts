584a1cdced12b44138c2fc4f6072b0ac573d1a73
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-05-21 14:28:20 -0400

    TableCellList components
    The current course-planner prototypes frequently use lists inside of a
    TableCell. I think it makes sense to have this as a special, Table
    specific design (as opposed to a more generalized List design), since
    this will probably be a pattern we employ in other circumstances.

--- src/Tables/index.ts
+++ src/Tables/index.ts
@@ -1,5 +1,7 @@
 export { default as Table } from './Table';
 export { default as TableCell, ALIGN } from './TableCell';
+export { default as TableCellList } from './TableCellList';
+export { default as TableCellListItem } from './TableCellListItem';
 export { default as TableRow } from './TableRow';
 export { default as TableHead } from './TableHead';
 export { default as TableHeadingCell } from './TableHeadingCell';
