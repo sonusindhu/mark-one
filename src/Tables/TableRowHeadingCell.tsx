d4b2ae9a5cd786a9706d5a9baec2dd732d3d93a4
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-05-27 12:14:15 -0400

    Add verticalAlignment prop to TableCells
    This will allow us to vertically align the text inside of the TableCell
    and TableRowHeadingCell components.

--- src/Tables/TableRowHeadingCell.tsx
+++ src/Tables/TableRowHeadingCell.tsx
@@ -3,11 +3,13 @@ import {
 } from 'react';
 import styled, { withTheme } from 'styled-components';
 import { BaseTheme } from '../Theme';
-import { ALIGN } from './TableCell';
+import { ALIGN, VALIGN } from './TableCell';
 
 export interface TableRowHeadingCellProps {
   /** Allows you to pass in a alignment property from the ALIGN enum */
   alignment?: ALIGN;
+  /** Allows for customizing the vertal alignment of the cell content */
+  verticalAlignment?: VALIGN;
   /** Specifies the background color of the table cell */
   backgroundColor?: string;
   /** Text or components to be displayed in the table heading cell */
@@ -22,16 +24,21 @@ export interface TableRowHeadingCellProps {
   theme?: BaseTheme;
 }
 
-const StyledTableHeadingCell = styled.th<TableRowHeadingCellProps>`
+const StyledTableRowHeadingCell = styled.th<TableRowHeadingCellProps>`
   border-left: ${({ theme }): string => (theme.border.light)};
   border-right: ${({ theme }): string => (theme.border.light)};
   font-weight: ${({ theme }): string => (theme.font.data.weight)};
   font-family: ${({ theme }): string => (theme.font.data.family)};
   font-size: ${({ theme }): string => (theme.font.data.size)};
   text-align: ${({ alignment }): string => alignment};
+  vertical-align: ${({ verticalAlignment }): string => verticalAlignment};
   background-color: ${({ backgroundColor }): string => backgroundColor};
 `;
 
+StyledTableRowHeadingCell.defaultProps = {
+  alignment: ALIGN.LEFT,
+  verticalAlignment: VALIGN.CENTER,
+};
 /**
  * @component
  * Renders a <th> element that is stylistically similar to the TableCell
@@ -39,7 +46,7 @@ const StyledTableHeadingCell = styled.th<TableRowHeadingCellProps>`
  */
 const TableRowHeadingCell: ForwardRefExoticComponent<
 TableRowHeadingCellProps
-> = withTheme(StyledTableHeadingCell);
+> = withTheme(StyledTableRowHeadingCell);
 
 declare type TableRowHeadingCell = ReactElement<TableRowHeadingCellProps>;
 
