5e4809fb6f1f4e0395827c6fec6bb7484e7826a8
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-07-14 14:52:03 -0400

    test for fromTheme

---
+++ src/Theme/__tests__/utils.test.ts
@@ -0,0 +1,29 @@
+import { strictEqual } from 'assert';
+import MarkOneTheme from '../MarkOneTheme';
+import * as utils from '../utils';
+
+describe('Theme Utilities', function () {
+  describe('fromTheme', function () {
+    const testProps = { theme: MarkOneTheme };
+    it('Should return a function to get string values from the theme', function () {
+      const getter = utils.fromTheme('ws', 'zero');
+      const result = getter(testProps);
+      strictEqual(result, MarkOneTheme.ws.zero);
+    });
+    it('Should return a function that handles any level of depth', function () {
+      const getter = utils.fromTheme('color', 'background', 'positive', 'medium');
+      const result = getter(testProps);
+      strictEqual(result, MarkOneTheme.color.background.positive.medium);
+    });
+    it('Should return a function that gets null instead of object value', function () {
+      const getter = utils.fromTheme('shadow');
+      const result = getter(testProps);
+      strictEqual(result, null);
+    });
+    it('Should return a function that gets null if the value is not defined', function () {
+      const getter = utils.fromTheme('foo', 'bar', 'baz');
+      const result = getter(testProps);
+      strictEqual(result, null);
+    });
+  });
+});
