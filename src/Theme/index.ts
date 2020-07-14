02ad1cfd0f3fe70bc37299d017a809d54beef704
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-07-14 14:53:43 -0400

    omit needless exports

--- src/Theme/index.ts
+++ src/Theme/index.ts
@@ -1,4 +1,2 @@
-export { default as GlobalCSS } from './GlobalCSS';
-export { default as MarkOneTheme, VARIANT } from './MarkOneTheme';
 export { default as MarkOneWrapper } from './MarkOneWrapper';
 export * from './utils';
