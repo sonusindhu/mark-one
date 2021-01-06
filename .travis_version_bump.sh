d5231cdcf7fe7531f2d66f91e03376169d7ac23e
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-01-06 12:07:47 -0500

    Handle version update on main and develop
    Replaces the simple if statement with a switch statement the
    conditionally sets which part of the version number should be
    incremented based on the current branch, or immediately and cleanly
    exits if run from any other branch.
    
    Also adds comments to explain the control flow in a little more detail

--- .travis_version_bump.sh
+++ .travis_version_bump.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" == develop ]]; then 
+case "$TRAVIS_BRANCH" in
+  main)
+    # Merges into "main" will update the minor version automatically
+    export NPM_TAG=stable
+    SEMVER_LEVEL=minor
+  ;;
+  develop)
+    # Merges into "Develop" will update the patch version automatically
+    export NPM_TAG=develop
+    SEMVER_LEVEL=patch
+  ;;
+  *)
+    # exit cleanly if on any other branch
+    exit 0
+  ;;
+esac
 
-  # Set the user name and email to match the API token holder
-  git config --local user.email $DEV_TEAM_EMAIL 
-  git config --local user.name "SEAS Computing Robot"
-  git config --local push.default matching
+# Set the user name and email to match the API token holder
+git config --local user.email $DEV_TEAM_EMAIL
+git config --local user.name "SEAS Computing Robot"
+git config --local push.default matching
 
-  # Get the credentials from a file
-  git config credential.helper "store --file=.git/credentials"
+# Get the credentials from a file
+git config credential.helper "store --file=.git/credentials"
 
-  # This associates the API Key with the account
-  echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-  
-  # Local branch must be clean before bumping version
-  git checkout -- .
+# This associates the API Key with the account
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 
-  npm version prerelease --preid=develop -m "[skip ci] Automatic bump to %s"
-  git push --tags origin HEAD:$TRAVIS_BRANCH
-fi
+# Local branch must be clean before bumping version
+git checkout -- .
 
+# Use npm's version command to:
+# 1. Increase the version number in package.json
+# 2. Create a new commit with the updated package.json, adding [skip ci] to
+#    prevent this script from running multiple times
+# 3. Add a git tag with the version number
+npm version $SEMVER_LEVEL -m "[skip ci] Automatic bump to %s"
+
+# Push the new new commit and the tag back into our github repo
+git push --tags origin HEAD:$TRAVIS_BRANCH
 
 # Adapted from https://dev.to/jeffreymfarley/deploy-atomically-with-travis--npm-68b
 # By Jeff Farley
