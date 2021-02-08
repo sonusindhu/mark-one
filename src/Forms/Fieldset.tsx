27a03df85e9984e31c092b47540cac7f75a1501e
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-02-08 16:45:00 -0500

    Remove the unnecessary margins from the Fieldset components
    This is causing the fieldset and the components below it to not line up vertically correctly.

--- src/Forms/Fieldset.tsx
+++ src/Forms/Fieldset.tsx
@@ -48,8 +48,6 @@ const StyledFieldset = styled.fieldset<StyledFieldsetProps>`
       ? `${theme.border.light}`
       : 'none'
   )};
-  margin-inline-start: 0.125em;
-  margin-inline-end: 0.125em;
   padding-block-start: 0.35em;
   padding-inline-start: 0.75em;
   padding-inline-end: 0.75em;
