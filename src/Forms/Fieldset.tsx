9d2e40c50ba167f90c40f9f9c0fbd9aeac819ec3
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-02-09 14:13:37 -0500

    Update the Fieldset component formatting
    This removes the padding-inline-start for cases in which the border and legends are not visible. This is what is causing the checkbox in the course planner course admin form to be indented compared to all other form fields.

--- src/Forms/Fieldset.tsx
+++ src/Forms/Fieldset.tsx
@@ -49,7 +49,11 @@ const StyledFieldset = styled.fieldset<StyledFieldsetProps>`
       : 'none'
   )};
   padding-block-start: 0.35em;
-  padding-inline-start: 0.75em;
+  padding-inline-start: ${({ isBorderVisible, isLegendVisible }): string => (
+    !isBorderVisible && !isLegendVisible
+    ? null
+    : '0.75em'
+  )};
   padding-inline-end: 0.75em;
   padding-block-end: 0.625em;
   min-inline-size: min-content;
