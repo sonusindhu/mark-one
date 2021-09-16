0485ec497b387cf623212295d39b51833a3c36c1
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-09-16 14:01:03 -0400

    Export ModalMessage from index.ts

--- src/Modals/index.ts
+++ src/Modals/index.ts
@@ -2,3 +2,4 @@ export { default as Modal } from './Modal';
 export { default as ModalBody } from './ModalBody';
 export { default as ModalFooter } from './ModalFooter';
 export { default as ModalHeader } from './ModalHeader';
+export { default as ModalMessage } from './ModalMessage';
